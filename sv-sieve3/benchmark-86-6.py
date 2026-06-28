5b31a874605e90ee6f7872027728457eb8732561
Stanislav Volodarskiy <svolodarskiy@nvidia.com>
2026-06-28 21:03:28 +0300

    changes in sv-sieve3/benchmark-86-6.py

--- sv-sieve3/benchmark-86-6.py
+++ sv-sieve3/benchmark-86-6.py
@@ -12,7 +12,10 @@ def main():
     step = 10_000_000_000
 
     scheme = collections.Counter()
-    for n2 in map(round, np.geomspace(1e6, 1e18, 13)):
+    p1 = 6
+    p2 = 20
+    f = 1
+    for n2 in map(round, np.geomspace(10 ** p1, 10 ** p2, 1 + f * (p2 - p1))):
         n1 = max(0, n2 - step)
         scheme.update({(n1, n2): 1})
 
